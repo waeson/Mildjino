e71488056bccd6748c93aa1702aaabbc939585fa
waeson <59735306+waeson@users.noreply.github.com>
2025-09-17 02:00:41 +0700

    загрузил МНГКМ

---
+++ data.js
@@ -0,0 +1,559 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "МНГКМ ВЖК обзорная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.055124275940388046,
+          "pitch": 0.20534907382649692,
+          "rotation": 3.141592653589793,
+          "target": "1---1-"
+        },
+        {
+          "yaw": -0.5156088179684133,
+          "pitch": 0.03640523533342588,
+          "rotation": 3.141592653589793,
+          "target": "7--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---1-",
+      "name": "МНГКМ коридор 1 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.8645860576919091,
+        "pitch": -0.029983736846268982,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.905307307027301,
+          "pitch": -0.039345195455579685,
+          "rotation": 3.141592653589793,
+          "target": "10---"
+        },
+        {
+          "yaw": -0.23588065521620294,
+          "pitch": -0.20545631183715152,
+          "rotation": 5.497787143782138,
+          "target": "2---2-"
+        },
+        {
+          "yaw": 2.000984357720389,
+          "pitch": 0.11621311583487604,
+          "rotation": 1.5707963267948966,
+          "target": "4---"
+        },
+        {
+          "yaw": -0.20717606078921058,
+          "pitch": 0.08309872583538791,
+          "rotation": 4.71238898038469,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---2-",
+      "name": "МНГКМ коридор 2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.404707868703543,
+        "pitch": 0.07636778283189827,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36306574275515757,
+          "pitch": 0.21201433338964293,
+          "rotation": 8.63937979737193,
+          "target": "1---1-"
+        },
+        {
+          "yaw": 1.4420558695612913,
+          "pitch": -0.16017031399178627,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -0.7407126401200603,
+          "pitch": -0.08768899523511209,
+          "rotation": 3.141592653589793,
+          "target": "8---"
+        },
+        {
+          "yaw": -2.5594939310000733,
+          "pitch": 0.04330090300260103,
+          "rotation": 4.71238898038469,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "МНГКМ бильярдная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.034626914473723,
+        "pitch": 0.28136832569024506,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4465329939264961,
+          "pitch": 0.1221971598509235,
+          "rotation": 4.71238898038469,
+          "target": "8---"
+        },
+        {
+          "yaw": -1.4604004549428442,
+          "pitch": -0.29163085907262243,
+          "rotation": 4.71238898038469,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "МНГКМ гостевой номер",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.2363186101578165,
+        "pitch": 0.49305284693603113,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.725123797047555,
+          "pitch": -0.17816295700416518,
+          "rotation": 6.283185307179586,
+          "target": "1---1-"
+        },
+        {
+          "yaw": 1.717690138690239,
+          "pitch": -0.024207015551365174,
+          "rotation": 4.71238898038469,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "МНГКМ консьердж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.36296092467429375,
+        "pitch": 0.048328062265110105,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9396467982981296,
+          "pitch": -0.18815948799711357,
+          "rotation": 0,
+          "target": "1---1-"
+        },
+        {
+          "yaw": 1.9385022040031572,
+          "pitch": 0.051917334421558436,
+          "rotation": 4.71238898038469,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "МНГКМ подсобка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.6007168181219242,
+        "pitch": 0.4012946680555274,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3966244294813492,
+          "pitch": -0.08167822146826254,
+          "rotation": 5.497787143782138,
+          "target": "2---2-"
+        },
+        {
+          "yaw": 2.4093685852123308,
+          "pitch": 0.08972291692633405,
+          "rotation": 1.5707963267948966,
+          "target": "1---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "МНГКМ прачечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.3279239969815997,
+        "pitch": 0.11806035906710122,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4610791156997127,
+          "pitch": -0.24268682561593558,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8---",
+      "name": "МНГКМ спортзал большой",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.697271233875064,
+        "pitch": 0.23064061896703691,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9308197986755342,
+          "pitch": -0.006176839942671819,
+          "rotation": 3.141592653589793,
+          "target": "2---2-"
+        },
+        {
+          "yaw": 0.8854794816359473,
+          "pitch": -0.036212217224878884,
+          "rotation": 3.141592653589793,
+          "target": "9---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---",
+      "name": "МНГКМ спортзал малый",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.3258084924501006,
+        "pitch": 0.5580873116182801,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.619897834392086,
+          "pitch": 0.00017920795892756303,
+          "rotation": 3.141592653589793,
+          "target": "8---"
+        },
+        {
+          "yaw": 0.32347659921796,
+          "pitch": -0.29525593726751787,
+          "rotation": 4.71238898038469,
+          "target": "2---2-"
+        },
+        {
+          "yaw": 0.35359074449608663,
+          "pitch": 0.1775818740238364,
+          "rotation": 1.5707963267948966,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10---",
+      "name": "МНГКМ столовая (качество)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.0510609253740295,
+        "pitch": 0.25975706716827673,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5413660787366172,
+          "pitch": -0.16399654921965734,
+          "rotation": 6.283185307179586,
+          "target": "1---1-"
+        },
+        {
+          "yaw": 1.5314680849936781,
+          "pitch": -0.016765579074499826,
+          "rotation": 4.71238898038469,
+          "target": "4---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "МНГКМ внутри",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
